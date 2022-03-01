6d26121d269847efa0eb7544dc1cd00377fc4079
John Erickson <johnterickson@github.com>
2022-03-01 15:54:14 -0800

    casting rays

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,86 @@
 mod vec3;
+use std::{ops::Sub, cmp::{self, min}};
+
 use vec3::*;
 
-struct Point(Vec3);
-struct Direction(Vec3);
-struct Ray(Point, Direction);
+struct Sphere(Point, f32);
+
+trait Intersect {
+    fn find_intersection(&self, r: &Ray) -> Option<f32>;
+}
+
+impl Intersect for Sphere {
+    // https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+    fn find_intersection(&self, r: &Ray) -> Option<f32> {
+        let O = r.0;
+        let D = r.1;
+        let C = self.0;
+        let R = self.1;
+
+        let O_minus_C = O - C;
+
+        let a = 1.0;
+        let b = 2.0 * D.dot(&O_minus_C);
+        let c = O_minus_C.dot(&O_minus_C) - R*R;
+
+        let discriminant = b*b - 4.0*a*c;
+        if discriminant < 0.0 {
+            return None;
+        } else if discriminant == 0.0 {
+            let t = b / (-2.0 * a);
+            Some(t)
+        } else {
+            let t0 = (discriminant.sqrt() - b) / (2.0 * a); 
+            let t1 = (discriminant.sqrt() + b) / (-2.0 * a); 
+            Some(if t0 < t1 {t0} else {t1})
+        }
+    }
+}
 
 fn main() {
     let camera_ray = Ray(
-        Point(Vec3 {
-            X: -10.0,
-            Y: -10.0,
-            Z: 0.0,
-        }),
-        Direction(
-            Vec3 {
-                X: 1.0,
-                Y: 1.0,
-                Z: 0.0,
-            }
-            .normalized(),
-        ),
+        Point(Vec3(-2.0, 0.0, 0.0)),
+        Direction(Vec3(1.0, 0.0, 0.0).normalized()),
     );
-    let camera_fov_degrees = 90.0;
+    let camera_up = Direction(Vec3(0.0, 0.0, 1.0));
+    let camera_right = camera_ray.1.cross(&camera_up);
+    println!("camera ray:{:?} right:{:?} up:{:?}",
+        &camera_ray, &camera_right, &camera_up);
 
     let imgx = 800;
     let imgy = 800;
 
+    let camera_w_fov_degrees: f32 = 90.0;
+    let camera_w_fov_radians: f32 = camera_w_fov_degrees.to_radians();
+    let caemra_h_fov_radians = camera_w_fov_radians * (imgy as f32) / (imgx as f32);
+    
+
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-    let center_x = imgx / 2;
-    let center_y = imgy / 2;
+    
+    let center_x = imgx as f32 / 2.0;
+    let center_y = imgy as f32 / 2.0;
+
+    let s = Sphere(Point(Vec3(0.0,0.0,0.0)), 1.0);
 
     // Iterate over the coordinates and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let i = (x + y) as f32;
-        let r = ((i / 1.0 as f32) % 256.0) as u8;
-        let g = ((i / 3.0 as f32) % 256.0) as u8;
-        let b = ((i / 7.0 as f32) % 256.0) as u8;
-        *pixel = image::Rgb([r, g, b]);
+        let radians_x = ((x as f32) - center_x) / (imgx as f32) * camera_w_fov_radians;
+        let radians_y = (center_y - (y as f32))  / (imgy as f32) * caemra_h_fov_radians;
+        let pixel_dir = 
+            camera_ray.1.0 + 
+            camera_right.0 * radians_x +
+            camera_up.0 * radians_y;
+        let pixel_dir = Direction(pixel_dir.normalized());
+        let pixel_ray = Ray(camera_ray.0, pixel_dir);
+
+        let color = if let Some(t) = s.find_intersection(&pixel_ray) {
+            let c = (100.0*t).clamp(0.0,255.0) as u8;
+            [c,c,c]
+        } else {
+            [0u8,0,0]
+        };
+        *pixel = image::Rgb(color);
     }
 
     imgbuf.save("out.png").unwrap();
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,22 +1,18 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign};
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
-pub struct Vec3 {
-    pub X: f32,
-    pub Y: f32,
-    pub Z: f32,
-}
+pub struct Vec3(pub f32, pub f32, pub f32);
 
 impl Vec3 {
     pub fn magnitude(&self) -> f32 {
-        (self.X * self.X + self.Y * self.Y + self.Z * self.Z).sqrt()
+        (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
     }
 
     pub fn normalize(&mut self) -> () {
         let mag = self.magnitude();
-        self.X /= mag;
-        self.Y /= mag;
-        self.Z /= mag;
+        self.0 /= mag;
+        self.1 /= mag;
+        self.2 /= mag;
     }
 
     pub fn normalized(mut self) -> Vec3 {
@@ -27,9 +23,9 @@ impl Vec3 {
 
 impl AddAssign for Vec3 {
     fn add_assign(&mut self, rhs: Self) {
-        self.X += rhs.X;
-        self.Y += rhs.Y;
-        self.Z += rhs.Z;
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+        self.2 += rhs.2;
     }
 }
 
@@ -45,9 +41,9 @@ impl Add for Vec3 {
 
 impl SubAssign for Vec3 {
     fn sub_assign(&mut self, rhs: Self) {
-        self.X -= rhs.X;
-        self.Y -= rhs.Y;
-        self.Z -= rhs.Z;
+        self.0 -= rhs.0;
+        self.1 -= rhs.1;
+        self.2 -= rhs.2;
     }
 }
 
@@ -58,5 +54,78 @@ impl Sub for Vec3 {
         let mut result = self;
         result -= rhs;
         result
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0 *= rhs;
+        self.1 *= rhs;
+        self.2 *= rhs;
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        let mut result = self;
+        result *= rhs;
+        result
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Point(pub Vec3);
+
+impl Sub for Point {
+    type Output = Direction;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Direction(self.0 - rhs.0)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Direction(pub Vec3);
+
+impl Direction {
+    pub fn dot(&self, rhs: &Self) -> f32 {
+        self.0.0 * rhs.0.0 + 
+        self.0.1 * rhs.0.1 +
+        self.0.2 * rhs.0.2
+    }
+    
+    pub fn cross(&self, rhs: &Self) -> Self {
+        Direction(Vec3(
+            self.0.1*rhs.0.2 - self.0.2*rhs.0.1,
+            self.0.2*rhs.0.0 - self.0.0*rhs.0.2,
+            self.0.0*rhs.0.1 - self.0.1*rhs.0.0
+        ))
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct Ray(pub Point, pub Direction);
+
+impl Ray {
+    fn from_points(from: Point, to: Point) -> Ray {
+        Ray(from, to - from)
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn add(){
+        assert_eq!(Vec3(1.0,2.0,3.0) + Vec3(4.0,6.0,9.0), Vec3(5.0,8.0,12.0));
+    }
+
+    #[test]
+    fn cross(){
+        assert_eq!(
+            Direction(Vec3(1.0,0.0,0.0)).cross(&Direction(Vec3(0.0,1.0,0.0))),
+            Direction(Vec3(0.0,0.0,1.0)));
     }
 }
