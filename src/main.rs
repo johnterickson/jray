df24de22484f9ab487f7b0a451303f2831a03e5f
John Erickson <johnterickson@github.com>
2022-03-04 08:24:25 -0800

    anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ pub struct Material {
     pub diffuse_color: Color,
     pub specular_color: Color,
     pub shininess: f64,
+    pub opacity: f64,
 }
 
 struct Light {
@@ -29,6 +30,18 @@ struct Intersection {
     pub material: Material,
 }
 
+enum Shape {
+    Sphere(Sphere)
+}
+
+impl Intersect for Shape {
+    fn find_intersection(&self, r: &Ray) -> Option<Intersection> {
+        match self {
+            Shape::Sphere(s) => s.find_intersection(r)
+        }
+    }
+}
+
 trait Intersect {
     fn find_intersection(&self, r: &Ray) -> Option<Intersection>;
 }
@@ -39,27 +52,58 @@ struct Camera {
     w_fov_degrees: f64,
 }
 
+struct AntiAliasing(Vec<(f64,f64)>);
+impl AntiAliasing {
+    fn create(n: u8) -> AntiAliasing {
+        let mut offsets = Vec::with_capacity(n as usize * n as usize);
+        offsets.push((0.0, 0.0));
+
+        let n: i8 = n.try_into().unwrap();
+
+        let delta = 1.0 / (n as f64 + 1.0);
+        for x in 1..n {
+            let x = x as f64;
+            for y in 1..n {
+                let y = y as f64;
+                offsets.push((x * delta, y * delta));
+                offsets.push((x * delta, -1.0 *y * delta));
+                offsets.push((-1.0 * x * delta, y * delta));
+                offsets.push((-1.0 * x * delta, -1.0 * y * delta));
+            }
+        }
+
+        AntiAliasing(offsets)
+    }
+
+    fn offsets(&self) -> &[(f64,f64)] {
+        self.0.as_slice()
+    }
+}
+
 struct Scene {
     camera: Camera,
     imgx: u32,
     imgy: u32,
-    shapes: Vec<Box<dyn Intersect + Send + Sync>>,
+    shapes: Vec<Shape>,
     lights: Vec<Light>,
 }
 
 impl Scene {
-    fn closest_intersection(&self, ray: &Ray) -> Option<Intersection> {
-        let intersections = self.shapes.iter().filter_map(|s| s.find_intersection(&ray));
-        let closest = intersections.min_by(|i1, i2| i1.distance.partial_cmp(&i2.distance).unwrap());
-        if let Some(ref closest) = closest {
-            assert!(closest.distance >= 0.0);
+    fn closest_intersection(&self, ray: &Ray) -> Option<(&Shape,Intersection)> {
+        let intersections = self.shapes.iter()
+            .filter_map(|s| s.find_intersection(&ray).map(|i| (s,i)));
+        let closest = intersections.min_by(|(_,i1), (_,i2)| {
+            i1.distance.partial_cmp(&i2.distance).unwrap()
+        });
+        if let Some((_, i)) = &closest {
+            assert!(i.distance >= 0.0);
         }
         closest
     }
 
     fn render_ray(&self, ray: &Ray) -> Color {
         let mut color = BLACK;
-        if let Some(i) = self.closest_intersection(&ray) {
+        if let Some( (_, i)) = self.closest_intersection(&ray) {
             color += Color(0.1, 0.1, 0.1); // ambient
 
             let slightly_off_surface = Point(i.point.0 + i.normal.0 * 0.00001);
@@ -129,6 +173,8 @@ impl Scene {
         let center_x = self.imgx as f64 / 2.0;
         let center_y = self.imgy as f64 / 2.0;
 
+        let aa = AntiAliasing::create(1);
+
         // Iterate over the coordinates and pixels of the image
         let mut pixels: Vec<_> = imgbuf.enumerate_pixels_mut().collect();
         pixels//.iter_mut()
@@ -137,21 +183,19 @@ impl Scene {
                 let mut color = BLACK;
 
                 let mut count = 0;
-                for xx in [-0.33, 0.0, 0.33] {
-                    for yy in [0.33, 0.0, 0.33] {
-                        let x = xx + *x as f64;
-                        let y = yy + *y as f64;
-                        let radians_x = (x - center_x) / (self.imgx as f64) * camera_w_fov_radians;
-                        let radians_y = (center_y - y) / (self.imgy as f64) * caemra_h_fov_radians;
-                        let pixel_dir = self.camera.ray.1.0
-                            + camera_right.0 * radians_x
-                            + self.camera.up.0 * radians_y;
-                        let pixel_dir = Direction(pixel_dir.normalized());
-                        let pixel_ray = Ray(self.camera.ray.0, pixel_dir);
-    
-                        color += self.render_ray(&pixel_ray);
-                        count += 1;
-                    }
+                for (xx, yy) in aa.offsets() {
+                    let x = xx + *x as f64;
+                    let y = yy + *y as f64;
+                    let radians_x = (x - center_x) / (self.imgx as f64) * camera_w_fov_radians;
+                    let radians_y = (center_y - y) / (self.imgy as f64) * caemra_h_fov_radians;
+                    let pixel_dir = self.camera.ray.1.0
+                        + camera_right.0 * radians_x
+                        + self.camera.up.0 * radians_y;
+                    let pixel_dir = Direction(pixel_dir.normalized());
+                    let pixel_ray = Ray(self.camera.ray.0, pixel_dir);
+
+                    color += self.render_ray(&pixel_ray);
+                    count += 1;
                 }
     
                 color *= 1.0 / count as f64;
@@ -164,41 +208,45 @@ impl Scene {
 }
 
 fn main() {
-    let shapes: Vec<Box<dyn Intersect + Send + Sync>> = vec![
-        Box::new(Sphere(
+    let shapes: Vec<Shape> = vec![
+        Shape::Sphere(Sphere(
             Point(Vec3(0.0, 0.0, 0.0)),
             0.7,
             Material {
                 diffuse_color: BLUE,
                 specular_color: 1.0 * WHITE,
                 shininess: 50.0,
+                opacity: 1.0,
             },
         )),
-        Box::new(Sphere(
+        Shape::Sphere(Sphere(
             Point(Vec3(-0.7, 0.7, -1.0)),
             1.0,
             Material {
                 diffuse_color: RED,
                 specular_color: 0.0 * WHITE,
                 shininess: 50.0,
+                opacity: 0.1,
             },
         )),
-        Box::new(Sphere(
+        Shape::Sphere(Sphere(
             Point(Vec3(1.0, -1.0, 1.0)),
             0.5,
             Material {
                 diffuse_color: GREEN,
                 specular_color: 0.5 * WHITE,
                 shininess: 50.0,
+                opacity: 1.0,
             },
         )),
-        Box::new(Sphere(
+        Shape::Sphere(Sphere(
             Point(Vec3(10.0, 0.0, -102.5)),
             100.0,
             Material {
                 diffuse_color: WHITE,
                 specular_color: 0.5 * WHITE,
                 shininess: 50.0,
+                opacity: 1.0,
             },
         )),
     ];
